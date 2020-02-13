c239fba623252fa617d890f5f42d5d5c8aa7bbba
Your Name <you@example.com>
2020-02-13 22:23:42 +1100

    update

--- hey.js
+++ hey.js
@@ -21,10 +21,28 @@ app.get('/result',(req,res)=>{
     var queryParameter=req.query;
     // res.json(queryParameter)
     if(!queryParameter.address&& !queryParameter.searchPlace){
-        return res.send({
+        // return res.send({
+        //     error:'You must provide the detail'
+        // })
+        return res.render('404',{
             error:'You must provide the detail'
         })
-    }
+    } 
+    else if(!queryParameter.address && queryParameter.searchPlace){
+        // return res.send({
+        //     error:'You must provide the detail'
+        // })
+        return res.render('404',{
+            error:'You must provide the address'
+        })
+    }else if(queryParameter.address && !queryParameter.searchPlace){
+        // return res.send({
+        //     error:'You must provide the detail'
+        // })
+        return res.render('404',{
+            error:'You must provide the place'
+        })
+    } else
     // else if(!queryParameter.address||!queryParameter.searchPlace){
     //     return res.send({
     //         error:'You must provide the detail2'
@@ -32,11 +50,17 @@ app.get('/result',(req,res)=>{
     // }
     geocode(queryParameter.address,(error,{ minLatitude, minLongitude, maxLatitude, maxLongitude }={})=>{
         if(error){
-            return res.send({ error })
+            // return res.send({ error })
+            return res.render('404',{
+                error
+            })
         }
         places(queryParameter.searchPlace,minLatitude, minLongitude, maxLatitude, maxLongitude,(error,PlaceSearch)=>{//callback function can be used within the callback function and let you use data from the other callback function. Need to watch out which parameters are used for which as parameter used for one callback function is extended to the callback function within. 
             if(error){
-                return res.send({ error })
+                return res.render('404',{
+                    error
+                })
+                // return res.send({ error })
             }
             // for(i=0;i<PlaceSearch().length;i++){
             //     res.send({
@@ -59,6 +83,7 @@ app.get('/result',(req,res)=>{
         })
     })
 })
+
 
 app.listen(port,()=>{// this finally sets the server
     console.log('Server is up on port '+port)
